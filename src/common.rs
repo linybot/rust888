7372321eda4bf748674ba7697eb029521c82853f
Jayce35-d <Jayce2370@protonmail.com>
2025-08-19 23:24:35 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://linybot.top:21114".to_owned()
+    "http://10.22.11.52:21114".to_owned()
 }
 
 #[inline]
