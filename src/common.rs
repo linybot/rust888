01a81db2016b62f5d1a44d91f79b278b6137869b
linybot <89718851+linybot@users.noreply.github.com>
2025-09-02 11:37:07 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://yczm.vatcp.cn:21120".to_owned()
+    "http://121.40.150.80:21114".to_owned()
 }
 
 #[inline]
